5ea1b23ce2fae8b6834ae9dffbe2061ca4871e73
om <patilomraje590@gmail.com>
2024-07-15 19:40:55 +0530

    l

---
+++ Task_Tracker/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Heading from './component/Heading'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Heading />
+     
+    </>
+  )
+}
+
+export default App
---
+++ Task_Tracker/src/component/Heading.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Heading() {
+  return (
+    <center>
+    <div class="row">
+    <div class="col col-lg-2">
+      <input type="text" placeholder='Enetr the task' />
+    </div>
+    <div class="col-md-auto">
+      <input type="date" name="" id="" />
+    </div>
+    <div class="col col-lg-2">
+    <button type="button" class="btn btn-success">Success</button>
+    </div>
+  </div>
+  </center>
+  )
+}
+
+export default Heading
